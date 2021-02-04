36c9d971f6ccad2a8fcfe8b11d89f265a5775910
ZXH <jialna@qq.com>
2021-02-05 02:36:58 +0800

    修改

--- config/config.js
+++ config/config.js
@@ -1,7 +1,4 @@
 // 配置服务器相关信息
 export default {
-  host: 'http://127.0.0.1:3000',
-  nwHost1: 'https://lakemusic.cn1.utools.club',
-  nwHost2: 'https://lakemusic2020.utools.club',
-  cloudLakeMusicHost: 'https://lakemusicapi.xn2001.com',
+  host: 'http://127.0.0.1:3000'
 }
